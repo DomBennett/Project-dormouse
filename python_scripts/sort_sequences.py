4a861f8612c0cec86803a40816419593a033a4a8
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-05-11 14:28:55 +0100

    Adapted to work with parts

--- python_scripts/sort_sequences.py
+++ python_scripts/sort_sequences.py
@@ -13,6 +13,7 @@ exclude = ['LM405059.1']  # list of Accessions to ignore
 # PACKAGES
 import os
 from Bio import SeqIO
+from parts import parts  # import dictionary of different sequence parts
 
 
 # FUNCTION
@@ -29,64 +30,64 @@ def getFasta(sequence, name=None):
     return(sequence.format('fasta'))
 
 
+def getIngroupSeqs(infile):
+    # read in GB ingroup sequences
+    sequences = []
+    with open(infile, 'rb') as f:
+        for record in SeqIO.parse(f, "gb"):
+            sequences.append(record)
+    # convert to fasta
+    sequences_fasta = []
+    for s in sequences:
+        if s.id not in exclude:
+            sequences_fasta.append(getFasta(s))
+    return(sequences_fasta)
+
+
+def getOutgroupSeq(infile):
+    if os.path.isfile(infile):
+        sequences = []
+        with open(infile, 'rb') as f:
+            for record in SeqIO.parse(f, "gb"):
+                sequences.append(record)
+        # choose one to add to sequences_gb
+        outgroup = sequences[selection]
+        # rename
+        outgroup = getFasta(outgroup, name='outgroup')
+        # add to sequences_fasta
+        return(outgroup)
+    else:
+        return(None)
+
 # DIRS
 output_dir = '1_sequences'
 if not os.path.isdir(output_dir):
     os.mkdir(output_dir)
+
 input_dir = 'data'
 blast_dir = os.path.join(input_dir, 'BLAST_results')
+sample_dir = os.path.join(input_dir, 'samples')
 
 # PROCESS
-# dict of infiles and outfiles
-files = {'p1': {'ingroup': os.path.join(blast_dir,
-                                        'p1_930F_Cyclophyllidea.gb'),
-                'outgroup': os.path.join(blast_dir,
-                                         'p1_cestode6_Caryophyllidea.gb'),
-                'sample': os.path.join(input_dir, 'p1.fasta'),
-                'outfile': os.path.join(output_dir, 'p1.fasta')},
-         'p2': {'ingroup': os.path.join(blast_dir,
-                                        'p2_600F_Cyclophyllidea.gb'),
-                'outgroup': os.path.join(blast_dir,
-                                         'p2_600F_Caryophyllidea.gb'),
-                'sample': os.path.join(input_dir, 'p2.fasta'),
-                'outfile': os.path.join(output_dir, 'p2.fasta')},
-         'p3': {'ingroup': os.path.join(blast_dir,
-                                        'p3_wormA_Cyclophyllidea.gb'),
-                'outgroup': os.path.join(blast_dir,
-                                         'p3_wormA_Caryophyllidea.gb'),
-                'sample': os.path.join(input_dir, 'p3.fasta'),
-                'outfile': os.path.join(output_dir, 'p3.fasta')}}
-for key in files.keys():
-    # read in GB ingroup sequences
-    sequences_gb = []
-    with open(files[key]['ingroup'], 'rb') as f:
-        for record in SeqIO.parse(f, "gb"):
-            sequences_gb.append(record)
-    # convert to fasta
-    sequences_fasta = []
-    for s in sequences_gb:
-        if s.id not in exclude:
-            sequences_fasta.append(getFasta(s))
+for key in parts.keys():
+    print key
+    print os.path.join(blast_dir, parts[key]['ingroup'])
+    if not os.path.isfile(os.path.join(blast_dir, parts[key]['ingroup'])):
+        next
+    sequences = getIngroupSeqs(os.path.join(blast_dir, parts[key]['ingroup']))
     # read in GB outgroup sequences
-    outgroup_sequences = []
-    with open(files[key]['outgroup'], 'rb') as f:
-        for record in SeqIO.parse(f, "gb"):
-            outgroup_sequences.append(record)
-    # choose one to add to sequences_gb
-    outgroup = outgroup_sequences[selection]
-    # rename
-    outgroup = getFasta(outgroup, name='outgroup')
-    # add to sequences_fasta
-    sequences_fasta.append(outgroup)
+    outgroup = getOutgroupSeq(os.path.join(blast_dir, parts[key]['outgroup']))
+    if outgroup:
+        sequences.append(outgroup)
     # read in primer pair samples
     samples = []
-    with open(files[key]['sample'], 'rb') as f:
+    with open(os.path.join(sample_dir, parts[key]['sample']), 'rb') as f:
         for record in SeqIO.parse(f, "fasta"):
             samples.append(record)
     # rename as 'sample' and add to sequences_fasta
     for sequence in samples:
-        sequences_fasta.append(getFasta(sequence, name='sample'))
+        sequences.append(getFasta(sequence, name='sample'))
     # write out as fasta
-    with open(files[key]['outfile'], 'wb') as f:
-        for s in sequences_fasta:
+    with open(os.path.join(output_dir, parts[key]['outfile']), 'wb') as f:
+        for s in sequences:
             f.write("{0}\n".format(s))
