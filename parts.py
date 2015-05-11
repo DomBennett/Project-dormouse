b7a8d6338a08027ad159faff64928191ac35e702
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-05-11 14:28:42 +0100

    Dictionary to hold sequence parts

---
+++ parts.py
@@ -0,0 +1,23 @@
+parts = {'18S_p1': {'ingroup': '18S_p1_930F_Cyclophyllidea.gb',
+                    'outgroup': '18S_p1_cestode6_Caryophyllidea.gb',
+                    'sample': '18S_p1.fasta',
+                    'outfile': '18S_p1.fasta'},
+         '18S_p2': {'ingroup': '18S_p2_600F_Cyclophyllidea.gb',
+                    'outgroup': '18S_p2_600F_Caryophyllidea.gb',
+                    'sample': '18S_p2.fasta',
+                    'outfile': '18S_p2.fasta'},
+         '18S_p3': {'ingroup': '18S_p3_wormA_Cyclophyllidea.gb',
+                    'outgroup': '18S_p3_wormA_Caryophyllidea.gb',
+                    'sample': '18S_p3.fasta',
+                    'outfile': '18S_p3.fasta'},
+         '28S_p1': {'ingroup': '28S_p1_Cyclophyllidea.gb',
+                    'outgroup': '28S_p1_Caryophyllidea.gb',
+                    'sample': '28S_p1.fasta',
+                    'outfile': '28S_p1.fasta'},
+         '28S_p2': {'ingroup': '28S_p2_Cyclophyllidea.gb',
+                    'outgroup': '28S_p2_Caryophyllidea.gb',
+                    'sample': '28S_p2.fasta',
+                    'outfile': '28S_p2.fasta'}}
+
+sample_ids_f = ['sample__930f', 'sample__600f', 'sample__wormA']
+sample_ids_r = ['sample__cestode6', 'sample__A27', 'sample__1270R']
