8e6668cd83fca8d4b3d1e9a2a0045832d63dafe5
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-05-20 14:34:13 +0100

    Extract features of interest

--- python_scripts/sort_sequences.py
+++ python_scripts/sort_sequences.py
@@ -12,6 +12,7 @@ exclude = ['LM405059.1']  # list of Accessions to ignore
 
 # PACKAGES
 import os
+import re
 from Bio import SeqIO
 from parts import parts  # import dictionary of different sequence parts
 
@@ -19,10 +20,6 @@ from parts import parts  # import dictionary of different sequence parts
 # FUNCTION
 def getFasta(sequence, name=None):
     '''Take SeqRecord and return fasta with name: "species_accession"'''
-    if not name:
-        # get sp name
-        name = sequence.annotations['organism']
-        name = name.replace(' ', '_')
     # add the accession
     name += '__' + sequence.id
     sequence.id = name
@@ -30,7 +27,22 @@ def getFasta(sequence, name=None):
     return(sequence.format('fasta'))
 
 
-def getIngroupSeqs(infile):
+def getFeature(sequence, name):
+    '''Return sequence of named feature if it exists'''
+    if not sequence.features:
+        return sequence
+    for f in sequence.features:
+        if 'product' in f.qualifiers.keys():
+            if re.search(name, f.qualifiers['product'][0]):
+                start = f.location.start.position
+                end = f.location.end.position
+                break
+    else:
+        return sequence
+    return sequence[start:end]
+
+
+def getIngroupSeqs(infile, feature_name):
     # read in GB ingroup sequences
     sequences = []
     with open(infile, 'rb') as f:
@@ -40,11 +52,14 @@ def getIngroupSeqs(infile):
     sequences_fasta = []
     for s in sequences:
         if s.id not in exclude:
-            sequences_fasta.append(getFasta(s))
+            name = s.annotations['organism']
+            name = name.replace(' ', '_')
+            s = getFeature(s, feature_name)
+            sequences_fasta.append(getFasta(s, name))
     return(sequences_fasta)
 
 
-def getOutgroupSeq(infile):
+def getOutgroupSeq(infile, feature_name):
     if os.path.isfile(infile):
         sequences = []
         with open(infile, 'rb') as f:
@@ -52,6 +67,8 @@ def getOutgroupSeq(infile):
                 sequences.append(record)
         # choose one to add to sequences_gb
         outgroup = sequences[selection]
+        # make sure you're getting the right feature
+        outgroup = getFeature(outgroup, feature_name)
         # rename
         outgroup = getFasta(outgroup, name='outgroup')
         # add to sequences_fasta
@@ -72,14 +89,15 @@ if __name__ == '__main__':
     # PROCESS
     for key in parts.keys():
         print key
+        gene = key.split('_')[0]
         print os.path.join(blast_dir, parts[key]['ingroup'])
         if not os.path.isfile(os.path.join(blast_dir, parts[key]['ingroup'])):
             next
         sequences = getIngroupSeqs(os.path.join(blast_dir,
-                                                parts[key]['ingroup']))
+                                                parts[key]['ingroup']), gene)
         # read in GB outgroup sequences
         outgroup = getOutgroupSeq(os.path.join(blast_dir,
-                                               parts[key]['outgroup']))
+                                               parts[key]['outgroup']), gene)
         if outgroup:
             sequences.append(outgroup)
         # read in primer pair samples
