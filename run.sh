2a76e479ec5542630471fd88ec7f393a31a84429
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-05-20 14:51:35 +0100

    Updated with new prints and logs

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 # Requires: mafft-qinsi, raxml, python2.7 and biopython
 # run by typing: `sh run.sh >& log.txt &`
 
+echo Started: $(date)
+
 # EXECUTABLE PATHS
 MAFFT=/home/djb208/bin/mafft-qinsi
 RAXML=$(which raxml)
@@ -15,34 +17,36 @@ ALIGN=${WD}/2_alignments/supermatrix.phy
 TREEOUT=${WD}/3_trees
 PART=${WD}/2_alignments/partitions.txt
 
-echo $WD
-echo $PART
-echo $ALIGN
-echo $TREEOUT
-
 # SORT SEQUENCES
+echo '\n1. Sorting sequences ....'
 $PYTHON python_scripts/sort_sequences.py
+echo 'Complete\n'
 
 # RUN ALIGNMENTS
+echo '2. Running MAFFT for ....'
 if [ ! -d 2_alignments ]; then
   mkdir 2_alignments
 fi
-for seq_file in `ls 1_sequences | grep \.fasta`
+for SF in `ls 1_sequences | grep \.fasta`
 do
-  echo "Running MAFFT for $seq_file ...."
-  $MAFFT 1_sequences/$seq_file > 2_alignments/${seq_file}_alignment.fasta 2> 2_alignments/${seq_file}_alignment_log.txt
+  echo ".... $SF"
+  $MAFFT 1_sequences/$seq_file > 2_alignments/${seq_file}_alignment.fasta 2> 2_alignments/${SF%.fasta}_log.txt
 done
-echo 'Complete'
+echo 'Complete\n'
 
-# # COMBINE ALIGNMENTS
+# COMBINE ALIGNMENTS
+echo '3. Combining alignments ....'
 $PYTHON python_scripts/combine.py
+echo 'Complete\n'
 
 # RUN RAXML
 if [ ! -d 3_trees ]; then
   mkdir 3_trees
 fi
-echo 'Running RAXML with bootstrap'
+echo '4. Running RAXML with bootstrap'
 $RAXML -n S1 -m GTRCAT -p $RANDOM -# 100 -s $ALIGN -T 2 -q $PART  -o outgroup -w $TREEOUT
 $RAXML -n S2 -m GTRCAT -p $RANDOM -b $RANDOM -# 100 -s $ALIGN -T 2 -q $PART -o outgroup -w $TREEOUT
 $RAXML -n S3 -m GTRCAT -p $RANDOM -f b -t ${TREEOUT}/RAxML_bestTree.S1 -z ${TREEOUT}/RAxML_bootstrap.S2 -w $TREEOUT
-echo 'Complete'
+echo 'Complete\n'
+
+echo Finished: $(date)
