33c5463cd444795f525438064a66aaaf36fdb3b6
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-05-20 14:34:30 +0100

    Minor edits + RAxML bootstrap

--- run.sh
+++ run.sh
@@ -6,13 +6,19 @@
 
 # EXECUTABLE PATHS
 MAFFT=/home/djb208/bin/mafft-qinsi
-RAXML=/usr/biosoft/bin/raxml
-PYTHON=/usr/local/python/2.7.5/bin/python
+RAXML=$(which raxml)
+PYTHON=$(which python)
 
 # FILES AND FOLDERS
-WD=$pwd
-ALIGN=$WD/2_alignments/supermatrix.phy
-TREEOUT=$WD/3_trees
+WD=$(pwd)
+ALIGN=${WD}/2_alignments/supermatrix.phy
+TREEOUT=${WD}/3_trees
+PART=${WD}/2_alignments/partitions.txt
+
+echo $WD
+echo $PART
+echo $ALIGN
+echo $TREEOUT
 
 # SORT SEQUENCES
 $PYTHON python_scripts/sort_sequences.py
@@ -24,17 +30,19 @@ fi
 for seq_file in `ls 1_sequences | grep \.fasta`
 do
   echo "Running MAFFT for $seq_file ...."
-  $MAFFT 1_sequences/$seq_file > 2_alignments/$seq_file_alignment.fasta 2> $seq_file_alignment_log.txt
+  $MAFFT 1_sequences/$seq_file > 2_alignments/${seq_file}_alignment.fasta 2> 2_alignments/${seq_file}_alignment_log.txt
 done
 echo 'Complete'
 
-# COMBINE ALIGNMENTS
+# # COMBINE ALIGNMENTS
 $PYTHON python_scripts/combine.py
 
 # RUN RAXML
 if [ ! -d 3_trees ]; then
   mkdir 3_trees
 fi
-echo 'Running RAXML'
-$RAXML -s $ALIGN -n tree -m GTRCAT -o outgroup -w $TREEOUT -T 2 -p $RANDOM
+echo 'Running RAXML with bootstrap'
+$RAXML -n S1 -m GTRCAT -p $RANDOM -# 100 -s $ALIGN -T 2 -q $PART  -o outgroup -w $TREEOUT
+$RAXML -n S2 -m GTRCAT -p $RANDOM -b $RANDOM -# 100 -s $ALIGN -T 2 -q $PART -o outgroup -w $TREEOUT
+$RAXML -n S3 -m GTRCAT -p $RANDOM -f b -t ${TREEOUT}/RAxML_bestTree.S1 -z ${TREEOUT}/RAxML_bootstrap.S2 -w $TREEOUT
 echo 'Complete'
