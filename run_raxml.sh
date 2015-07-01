b476822a7dd0ea32fc2118405386cc4ed3dfeb07
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-07-01 12:01:01 +0100

    Make outgroup and part optional

--- run_raxml.sh
+++ run_raxml.sh
@@ -15,9 +15,10 @@ ALIGN=${WD}/2_alignments/supermatrix.phy
 TREEOUT=${WD}/3_trees/
 PART=${WD}/2_alignments/partitions.txt
 
+# add -o outgroup and -q $PART if need be
 echo '4. Running RAXML with bootstrap'
-$RAXML -n S1 -m GTRCAT -p $RANDOM -# 100 -s $ALIGN -q $PART  -o outgroup -w $TREEOUT
-$RAXML -n S2 -m GTRCAT -p $RANDOM -b $RANDOM -# 100 -s $ALIGN -q $PART -o outgroup -w $TREEOUT
+$RAXML -n S1 -m GTRCAT -p $RANDOM -# 100 -s $ALIGN -w $TREEOUT
+$RAXML -n S2 -m GTRCAT -p $RANDOM -b $RANDOM -# 100 -s $ALIGN -w $TREEOUT
 $RAXML -n S3 -m GTRCAT -p $RANDOM -f b -t ${TREEOUT}RAxML_bestTree.S1 -z ${TREEOUT}RAxML_bootstrap.S2 -w $TREEOUT
 echo 'Complete\n'
 
